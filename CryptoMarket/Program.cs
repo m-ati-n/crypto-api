6a7c05b73d3b00918bad47ffa599d8465b049820
matin <matinnaderi211@gamil.com>
2026-07-20 15:57:41 +0330

    fix

--- CryptoMarket/Program.cs
+++ CryptoMarket/Program.cs
@@ -47,6 +47,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
